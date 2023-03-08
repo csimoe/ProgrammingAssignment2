fbea896a4a0d84e8a0d6254399a56072a72f6692
csimoe <csimoes.1@alumni.unav.es>
2023-03-08 18:24:40 +0000

    Update cachematrix.R
    Added complete functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,28 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(1:800, nrow=8, ncol=100)){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function () {x}
+  Inverse_function <- function(inverse) {inv <<- inverse}
+  Get_Inverse <- function () {inv}
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  inv <- x$Get_Inverse
+  if (!is.null(inv)){
+    message("Getting cache data")
+    return(inv)
+  }
+  matt <- x$get()
+  inv <- solve(mat, ...)
+  x$Inverse_function(inv)
+  inv
 }
